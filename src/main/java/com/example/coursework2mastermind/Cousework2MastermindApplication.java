9f1a3f9319c6f91c7b53b0db3cb9fa8a9fa536fc
Макс <maxim__90.90@mail.ru>
2023-06-08 15:47:38 +0400

    first commit

--- src/main/java/com/example/coursework2mastermind/Cousework2MastermindApplication.java
+++ src/main/java/com/example/coursework2mastermind/Cousework2MastermindApplication.java
@@ -1,4 +1,4 @@
-package com.example.cousework2mastermind;
+package com.example.coursework2mastermind;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
